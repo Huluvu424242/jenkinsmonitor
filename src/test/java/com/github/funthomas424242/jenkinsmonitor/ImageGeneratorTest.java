1faedc3ab8b148ca6d857933769991baaf60006f
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-18 19:07:56 +0200

    Test graues Image 100x100 grün - mit Übergabe leeren JobStatus
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/ImageGeneratorTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/ImageGeneratorTest.java
@@ -13,10 +13,19 @@ public class ImageGeneratorTest {
 
 
     @Test
-    @DisplayName("Generiert ein graues Icon 100x100")
-    void createGrayIcon100x100() {
-//        final JobStatus[] jobsStatus = new JobStatus[0];
+    @DisplayName("Initialisierung mit null generiert ein graues Icon 100x100")
+    void initNullCreateGrayIcon100x100() {
         final ImageGenerator generator = new ImageGenerator(null);
+        final BufferedImage image = generator.getImage(100,100);
+        assertEquals(100,image.getHeight());
+        assertEquals(100,image.getWidth());
+    }
+
+    @Test
+    @DisplayName("Initialisierung ohne Jobs generiert ein graues Icon 100x100")
+    void initEmptyCreateGrayIcon100x100() {
+        final JobStatus[] jobsStatus = new JobStatus[0];
+        final ImageGenerator generator = new ImageGenerator(jobsStatus);
         final BufferedImage image = generator.getImage(100,100);
         assertEquals(100,image.getHeight());
         assertEquals(100,image.getWidth());
