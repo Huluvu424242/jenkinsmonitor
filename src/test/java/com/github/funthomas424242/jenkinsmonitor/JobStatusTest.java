6a917c813fe15305b21cc638d1051d651b5164e4
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-18 20:15:17 +0200

    Tests für JobStatus grün, gelb, rot und grau  auf grün
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/JobStatusTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/JobStatusTest.java
@@ -1,5 +1,27 @@
 package com.github.funthomas424242.jenkinsmonitor;
 
+/*-
+ * #%L
+ * Jenkins Monitor
+ * %%
+ * Copyright (C) 2019 PIUG
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +30,7 @@ import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JobStatusTest {
 
@@ -17,13 +38,24 @@ class JobStatusTest {
     protected static URL LOCALHOST_JOB_TEST_URL;
 
     @BeforeAll
-    void setUp() throws MalformedURLException {
+    static void  setUp() throws MalformedURLException {
         new URL("http://localhost:8090/job/test");
     }
 
     @Test
     @DisplayName("Es wird eine gültige Instanz erstellt")
     void valideInitialisierung() {
+        final JobStatus jobStatus = new JobStatus(null
+            , null
+            , null);
+        assertNotNull(jobStatus);
+//        assertNull(jobStatus.get)
+        assertEquals(Color.gray,jobStatus.getStatusColor());
+    }
+
+    @Test
+    @DisplayName("Statusfarbe eines erfolgreichen Jobs ist grün")
+    void erfolgreicheJobsSindGruen() {
         final JobStatus jobStatus = new JobStatus(NAME_JOB1
             , JobStatus.Status.SUCCESS
             , LOCALHOST_JOB_TEST_URL);
@@ -31,4 +63,23 @@ class JobStatusTest {
         assertEquals(Color.green,jobStatus.getStatusColor());
     }
 
+    @Test
+    @DisplayName("Statusfarbe eines instabilen Jobs ist gelb")
+    void instabileJobsSindGelb() {
+        final JobStatus jobStatus = new JobStatus(NAME_JOB1
+            , JobStatus.Status.INSTABIL
+            , LOCALHOST_JOB_TEST_URL);
+        assertNotNull(jobStatus);
+        assertEquals(Color.yellow,jobStatus.getStatusColor());
+    }
+
+    @Test
+    @DisplayName("Statusfarbe eines fehlerhaften Jobs ist rot")
+    void fehlerhafteJobsSindRot() {
+        final JobStatus jobStatus = new JobStatus(NAME_JOB1
+            , JobStatus.Status.FAILED
+            , LOCALHOST_JOB_TEST_URL);
+        assertNotNull(jobStatus);
+        assertEquals(Color.red,jobStatus.getStatusColor());
+    }
 }
