a563fc6bb26fc0fa1f0d213bebb118b98f571fde
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-19 21:04:50 +0200

    Refactoring: Prüfung Image Color in Methode ausgelagert und obsolete Tests aus Testbacklog entfernt
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsMonitorTrayTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsMonitorTrayTest.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.jenkinsmonitor;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -31,6 +31,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JenkinsMonitorTrayTest {
 
@@ -42,21 +43,25 @@ public class JenkinsMonitorTrayTest {
         jenkinsMonitorTray.show();
     }
 
+    boolean isImageOfColor(BufferedImage image, Color color) {
+        boolean isOfColor = true;
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        for (int breite = 0; breite < width; breite++) {
+            for (int hoehe = 0; hoehe < height; hoehe++) {
+                isOfColor = isOfColor && image.getRGB(breite, hoehe) == color.getRGB();
+            }
+        }
+        return isOfColor;
+    }
 
     @Test
-    @DisplayName("Initial zeigt der Tooltipp <<No jobs watching>>")
+    @DisplayName("Initial wird ein graues Icon angezeigt mit Tooltipp <<No jobs watching>>")
     public void shouldShowNoJobsWatching() throws AWTException {
         final TrayIcon trayIcon = jenkinsMonitorTray.getTrayIcon();
         assertEquals("No jobs watching", trayIcon.getToolTip());
-    }
-
-
-    @Test
-    @Disabled
-    @DisplayName("Erzeuge ein graues TrayIcon mit Tooltipp: No jobs watching")
-    public void createNoJobsWatching() {
-        final TrayIcon trayIcon = jenkinsMonitorTray.getTrayIcon();
-        assertEquals("No jobs watching", trayIcon.getToolTip());
+        final Image image = trayIcon.getImage();
+        assertTrue(isImageOfColor((BufferedImage) image, JobStatusBeschreibung.JobStatus.OTHER.getColor()));
     }
 
 
