8535b7c7312d41e02eddc24b32d3ab20d23ba3db
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-20 21:33:44 +0200

    Refactoring: obsolete Imports entfernt
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsAPIMock.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsAPIMock.java
@@ -1,7 +1,32 @@
 package com.github.funthomas424242.jenkinsmonitor;
 
+/*-
+ * #%L
+ * Jenkins Monitor
+ * %%
+ * Copyright (C) 2019 PIUG
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 
 public class JenkinsAPIMock {
 
@@ -18,20 +43,20 @@ public class JenkinsAPIMock {
 
 
     public static void definiereAnnahmen(WireMockServer jenkins) {
-        jenkins.stubFor(WireMock.get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_RED))
-            .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
+        jenkins.stubFor(get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_RED))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
                 .withStatus(200)
                 .withBodyFile("json/multibranch-job1-red.json")));
-        jenkins.stubFor(WireMock.get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_GREEN))
-            .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
+        jenkins.stubFor(get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_GREEN))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
                 .withStatus(200)
                 .withBodyFile("json/multibranch-job1-green.json")));
-        jenkins.stubFor(WireMock.get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_YELLOW))
-            .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
+        jenkins.stubFor(get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_YELLOW))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
                 .withStatus(200)
                 .withBodyFile("json/multibranch-job1-yellow.json")));
-        jenkins.stubFor(WireMock.get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_GRAY))
-            .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
+        jenkins.stubFor(get(WireMock.urlEqualTo(STATUSPATH_MULTIBRANCH_JOB1_GRAY))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
                 .withStatus(200)
                 .withBodyFile("json/multibranch-job1-gray.json")));
     }
