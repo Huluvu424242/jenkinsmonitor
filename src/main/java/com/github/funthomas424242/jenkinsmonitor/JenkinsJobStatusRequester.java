1fc84833f57637951aa9ab80d9d95a7de64a7311
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-20 17:18:56 +0200

    Trviale Lösung implementiert
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/JenkinsJobStatusRequester.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/JenkinsJobStatusRequester.java
@@ -1,7 +1,10 @@
 package com.github.funthomas424242.jenkinsmonitor;
 
+import java.net.URL;
+
 public class JenkinsJobStatusRequester {
-    public Object getJobStatus() {
-        return null;
+
+    public Object getJobStatus(final URL jenkinsJobURL) {
+        return "{hallo}";
     }
 }
--- src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsJobStatusRequesterTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/JenkinsJobStatusRequesterTest.java
@@ -1,7 +1,11 @@
 package com.github.funthomas424242.jenkinsmonitor;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -9,12 +13,19 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 public class JenkinsJobStatusRequesterTest {
 
 
+    protected static URL urlMultibranchJob1;
+
+    @BeforeAll
+    static void setUp() throws MalformedURLException {
+        urlMultibranchJob1 = new URL("http://stachel:8090/job/multibranchjob1/job/master");
+    }
+
     @Test
     @DisplayName("Valid Request erzeugt gültigen Response")
     protected void validInstanz(){
         final JenkinsJobStatusRequester requester = new JenkinsJobStatusRequester();
         assumeTrue(requester!=null);
-//        assertNotNull(requester.getJobStatus());
+        assertNotNull(requester.getJobStatus(urlMultibranchJob1));
     }
 
 }
