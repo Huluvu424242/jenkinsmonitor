9bc5cdc4d38edd852f347333c5f44a957a451924
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-22 11:43:18 +0200

    Refactoring: LoggerBinding added, API as interface for codecoverage
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/JenkinsAPI.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/JenkinsAPI.java
@@ -22,7 +22,6 @@ package com.github.funthomas424242.jenkinsmonitor;
  * #L%
  */
 
-public abstract class JenkinsAPI {
-    public static final String STATUS_PATH = "/lastBuild/api/json";
-
+public interface JenkinsAPI {
+    String STATUS_PATH = "/lastBuild/api/json";
 }
