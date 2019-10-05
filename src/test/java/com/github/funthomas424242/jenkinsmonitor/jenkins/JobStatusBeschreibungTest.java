9ebdc78754f4d8ff98b0a8e0fc20e23f688ef02c
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-10-05 11:50:36 +0200

    codacy issues fixed
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/jenkins/JobStatusBeschreibungTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/jenkins/JobStatusBeschreibungTest.java
@@ -23,7 +23,6 @@ package com.github.funthomas424242.jenkinsmonitor.jenkins;
  */
 
 import nl.jqno.equalsverifier.EqualsVerifier;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
