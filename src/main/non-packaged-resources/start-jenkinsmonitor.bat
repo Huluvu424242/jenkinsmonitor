a96937f17cc9188d971923a60c4b870184cb4bb2
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-10-06 17:29:41 +0200

    Startskripte aufgenommen
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

---
+++ src/main/non-packaged-resources/start-jenkinsmonitor.bat
@@ -0,0 +1,9 @@
+# startet den jenkinsmonitor
+#
+# Konfigurationsfile im Home des Nutzers
+# ~\jenkinsmonitor.properties
+#
+# Systemanforderungen: Java 8 muss installiert sein
+#
+#
+start javaw -jar ${project.build.finalName}-jar-with-dependencies.jar
---
+++ src/main/non-packaged-resources/start-jenkinsmonitor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S java -jar ${project.build.finalName}-jar-with-dependencies.jar
+
+# startet den jenkinsmonitor
+#
+# Konfigurationsfile im Home des Nutzers
+# ~\jenkinsmonitor.properties
+#
+# Systemanforderungen: Java 8 muss installiert sein
+#
+#
+
