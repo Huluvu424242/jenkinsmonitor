a18cc32f1d5e134498f714f8036262e5108b3aa2
Thomas Schubert <funthomas424242@gmail.com>
2019-10-13 14:02:39 +0200

    [#25] Test grün

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/jenkins/StatusAbfrageInformationen.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/jenkins/StatusAbfrageInformationen.java
@@ -1,28 +1,44 @@
 package com.github.funthomas424242.jenkinsmonitor.jenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.util.Base64;
 
-final public class StatusAbfrageInformationen {
-    public final URL statusAbfrageUrl;
-    public String basicAuthToken;
+public class StatusAbfrageInformationen {
 
-    public StatusAbfrageInformationen(final URL statusAbfrageUrl, final String userName, String password){
-        this.statusAbfrageUrl=statusAbfrageUrl;
+    protected final Logger LOGGER = LoggerFactory.getLogger(StatusAbfrageInformationen.class);
+
+    protected final URL statusAbfrageUrl;
+    protected final String userName;
+    protected final String password;
+
+    public StatusAbfrageInformationen(final URL statusAbfrageUrl, final String userName, String password) {
+        this.statusAbfrageUrl = statusAbfrageUrl;
+        this.userName = userName;
+        this.password = password;
     }
 
     public URL getStatusAbfrageUrl() {
-        return null;
+        return statusAbfrageUrl;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 
     public String getBasicAuthToken(String password) {
+        try {
+            return Base64.getEncoder().encodeToString(String.format("%s:%s", userName, password).getBytes("utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("Nichtunterstütztes Encoding für das Verschlüsseln der Zugangsdaten angefordert");
+        }
         return null;
     }
 }
