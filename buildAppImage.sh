03981f7bfc6a7f65d1e979d794049293d722d695
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-11-06 18:00:45 +0100

    [#27_appimage] Buildskripte für appimage erstellt lauffähig
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- buildAppImage.sh
+++ buildAppImage.sh
@@ -2,12 +2,6 @@
 
 export ARCH=x86_64
 
-# copy jar
-#mkdir -p AppDir/usr/share/jenkinsmonitor
-cp -a ./target/jenkinsmonitor-*-jar-with-dependencies.jar AppDir/jenkinsmonitor-jar-with-dependencies.jar
-
-#read -p "Press [Enter] Jenkinsmonitor jar platziert  ..."
-
 # download jre 11
 wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.3/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
 tar xf OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
@@ -26,8 +20,8 @@ cp -Rf usr AppDir/
 # download AppImageKit and give permissions und run it
 wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
-#wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-#chmod +x linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
 
 #echo "Starte Workarounds"
 # worarounds for missing deps
@@ -35,18 +29,23 @@ chmod +x appimagetool-x86_64.AppImage
 #cp -a ./usr/lib/compressedrefs/libj9thr29.so AppDir/usr/lib/libj9thr29.so
 #read -p "Press [Enter] Workarounds erledigt ..."
 
+# copy resources at final place
+cp -a ./target/jenkinsmonitor-*-jar-with-dependencies.jar AppDir/usr/bin/jenkinsmonitor-jar-with-dependencies.jar
+cp -Rf appimg-resources/* AppDir/
+ln -s AppDir/AppRun AppDir/usr/bin/jenkinsmonitor.wrapper
+
+
 # loesche altes Image und erstelle neu
 rm Jenkins_Monitor*.AppImage
 ./appimagetool-x86_64.AppImage AppDir/
+#./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 #./linuxdeploy-x86_64.AppImage --appdir AppDir -e target/jenkinsmonitor-0.0.2-SNAPSHOT-jar-with-dependencies.jar -i appimg-resources/1984EmmanuelGoldstein.png -d appimg-resources/myapp.desktop  --output appimage
 #./linuxdeploy-x86_64.AppImage --appdir AppDir -e appimg-resources/launch.sh -i appimg-resources/1984EmmanuelGoldstein.png -d appimg-resources/myapp.desktop  --output appimage
-#./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 
 #read -p "Press [Enter] to start remove waste files ..."
 rm ./linuxdeploy*.AppImage
-rm ./AppDir/.DirIcon
-rm ./AppDir/*.jar
-rm -rf ./AppDir/usr
+rm -rf ./AppDir/*
+rm -rf ./AppDir/.DirIcon
 rm -rf ./usr
 
 
