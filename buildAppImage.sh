7ef148e3e6cb8628d8fb07a81d98d480ed9b8cae
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-11-06 15:33:50 +0100

    [#27_appimage] Buildskripte für appimage erstellt
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- buildAppImage.sh
+++ buildAppImage.sh
@@ -3,34 +3,49 @@
 export ARCH=x86_64
 
 # copy jar
-mkdir -p AppDir/usr/share/jenkinsmonitor
-cp -a ./target/jenkinsmonitor-*-jar-with-dependencies.jar AppDir/usr/share/jenkinsmonitor/jenkinsmonitor-jar-with-dependencies.jar
+#mkdir -p AppDir/usr/share/jenkinsmonitor
+cp -a ./target/jenkinsmonitor-*-jar-with-dependencies.jar AppDir/jenkinsmonitor-jar-with-dependencies.jar
 
-# read -p "Press [Enter]  ..."
+#read -p "Press [Enter] Jenkinsmonitor jar platziert  ..."
 
 # download jre 11
 wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.3/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
 tar xf OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
 
 # list deps modules
-./jdk-11.0.3+7/bin/jdeps  --list-deps AppDir/usr/share/jenkinsmonitor/jenkinsmonitor-jar-with-dependencies.jar
+./jdk-11.0.3+7/bin/jdeps  --list-deps ./target/jenkinsmonitor-*-jar-with-dependencies.jar
 
-# os specific binary image creating via jlink
+# spezielles jre bauen
 ./jdk-11.0.3+7/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.security.jgss,java.sql,java.xml --output usr
 
 # copy binary
 cp -Rf usr AppDir/
 
+#read -p "Press [Enter] jre gebaut ..."
+
 # download AppImageKit and give permissions und run it
 wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
+#wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+#chmod +x linuxdeploy-x86_64.AppImage
+
+#echo "Starte Workarounds"
+# worarounds for missing deps
+#cp -a ./usr/lib/j9vm/libjvm.so AppDir/usr/lib/libjvm.so
+#cp -a ./usr/lib/compressedrefs/libj9thr29.so AppDir/usr/lib/libj9thr29.so
+#read -p "Press [Enter] Workarounds erledigt ..."
 
 # loesche altes Image und erstelle neu
 rm Jenkins_Monitor*.AppImage
 ./appimagetool-x86_64.AppImage AppDir/
+#./linuxdeploy-x86_64.AppImage --appdir AppDir -e target/jenkinsmonitor-0.0.2-SNAPSHOT-jar-with-dependencies.jar -i appimg-resources/1984EmmanuelGoldstein.png -d appimg-resources/myapp.desktop  --output appimage
+#./linuxdeploy-x86_64.AppImage --appdir AppDir -e appimg-resources/launch.sh -i appimg-resources/1984EmmanuelGoldstein.png -d appimg-resources/myapp.desktop  --output appimage
+#./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 
-read -p "Press [Enter] to start remove waste files ..."
+#read -p "Press [Enter] to start remove waste files ..."
+rm ./linuxdeploy*.AppImage
 rm ./AppDir/.DirIcon
+rm ./AppDir/*.jar
 rm -rf ./AppDir/usr
 rm -rf ./usr
 
