a0683d1d9540589b6270261975678963d74a8e36
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-02 00:03:35 +0200

    path generatlisiert

--- runlocal.sh
+++ runlocal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/lib/jvm/java-13-openjdk/bin/java -Dlogback.configurationFile=filelogging.xml -jar target/jenkinsmonitor-0.0.2-SNAPSHOT-jar-with-dependencies.jar
+${JAVA_HOME}/bin/java -Dlogback.configurationFile=filelogging.xml -jar target/jenkinsmonitor-0.0.2-SNAPSHOT-jar-with-dependencies.jar
 
