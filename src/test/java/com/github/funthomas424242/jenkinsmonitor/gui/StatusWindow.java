42d7b9dd400893b330729f63f3c73f691b3a1377
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-02 22:57:30 +0200

    Refactoring createLabel

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/gui/StatusWindow.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/gui/StatusWindow.java
@@ -1,10 +1,20 @@
 package com.github.funthomas424242.jenkinsmonitor.gui;
 
-import java.awt.BorderLayout;
+import com.github.funthomas424242.jenkinsmonitor.etc.Counter;
+import com.github.funthomas424242.jenkinsmonitor.jenkins.JobStatusBeschreibung;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class StatusWindow extends JWindow {
+    public static final Logger LOGGER = LoggerFactory.getLogger(StatusWindow.class);
 
 //    JScrollPane scrollpane;
 
@@ -15,17 +25,42 @@ public class StatusWindow extends JWindow {
         aktualisiereContentPane();
     }
 
+
+    private JLabel createLabel(final Counter counter, JobStatusBeschreibung jobStatus) {
+        final String htmlTemplate = "<html><body style=\"display:inline-block;\"><h1>[" + jobStatus.getOrderId() + "] " + jobStatus.getJobName() + "</h1>"
+            + "<p>(" + counter.value + ") Status: " + jobStatus.getJobStatus().toString()
+            + " <a href=\"" + jobStatus.getJobUrl() + "\">" + jobStatus.getJobUrl() + "</a></p></body></html>";
+        final JLabel label = new JLabel(htmlTemplate);
+        label.setOpaque(true);
+        label.setBackground(jobStatus.getStatusColor());
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                URI uri = null;
+                try {
+                    uri = jobStatus.getJobUrl().toURI();
+                    Desktop.getDesktop().browse(uri);
+                    setVisible(false);
+                } catch (IOException | URISyntaxException ex) {
+                    LOGGER.error("URL " + uri.toString() + " konnte nicht geöffnet werden", ex);
+                }
+            }
+        });
+        return label;
+    }
+
+
     private JScrollPane createContent() {
-        String categories[] = { "Household", "Office", "Extended Family",
+        String categories[] = {"Household", "Office", "Extended Family",
             "Company (US)", "Company (World)", "Team", "Will",
             "Birthday Card List", "High School", "Country", "Continent",
-            "Planet" };
+            "Planet"};
         JList list = new JList(categories);
         return new JScrollPane(list);
     }
 
-    public void aktualisiereContentPane(){
-        getContentPane().add( createContent(), BorderLayout.CENTER);
+    public void aktualisiereContentPane() {
+        getContentPane().add(createContent(), BorderLayout.CENTER);
     }
 
     public static void main(String args[]) {
