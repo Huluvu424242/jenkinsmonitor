f7c5a5c5f7bd4e56a4546114f11fcfce91fff65a
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-08 16:45:18 +0200

    [feature/parallel-requests] Gemeinschaftsinterface zwischen JobBeschreibung und JobStatusBeschreibung erstellt
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

---
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/jenkins/AbstractJobBeschreibung.java
@@ -0,0 +1,52 @@
+package com.github.funthomas424242.jenkinsmonitor.jenkins;
+
+/*-
+ * #%L
+ * Jenkins Monitor
+ * %%
+ * Copyright (C) 2019 - 2020 PIUG
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import net.greypanther.natsort.SimpleNaturalComparator;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URL;
+import java.util.Comparator;
+
+public interface AbstractJobBeschreibung extends Comparable<JobStatusBeschreibung> {
+
+    Comparator<String> NATURAL_COMPARATOR = SimpleNaturalComparator.getInstance();
+
+    URL getJobUrl();
+
+    String getJobOrderId();
+
+    default String getPrimaryKey() {
+        final String url = getJobUrl() != null ? getJobUrl().toExternalForm() : "";
+        return getJobOrderId() + "#" + url;
+    }
+
+    default int compareTo(@NotNull AbstractJobBeschreibung jobBeschreibung) {
+        if (getJobOrderId() == null && jobBeschreibung.getJobOrderId() == null) return 0;
+        if (getJobOrderId() != null && jobBeschreibung.getJobOrderId() == null) return 1;
+        if (getJobOrderId() == null && jobBeschreibung.getJobOrderId() != null) return -1;
+
+        return NATURAL_COMPARATOR.compare(getJobOrderId(), jobBeschreibung.getJobOrderId());
+    }
+
+}
