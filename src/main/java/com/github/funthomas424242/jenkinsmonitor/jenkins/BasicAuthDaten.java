c4043eef5959e41c6bf956ce7d7fb216fb77b389
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-21 22:42:25 +0200

    [feature/labelbreite] Refactoring: Clean Code
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/jenkins/BasicAuthDaten.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/jenkins/BasicAuthDaten.java
@@ -10,29 +10,23 @@ package com.github.funthomas424242.jenkinsmonitor.jenkins;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Objects;
 
 public class BasicAuthDaten {
-
-    transient protected final Logger LOGGER = LoggerFactory.getLogger(BasicAuthDaten.class);
 
     protected String userName;
 
