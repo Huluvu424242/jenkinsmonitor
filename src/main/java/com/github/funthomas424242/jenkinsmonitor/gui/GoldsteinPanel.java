beb861c152c6814413c310709563c69136245ffb
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-21 22:39:27 +0200

    [feature/labelbreite] Refactoring: Clean Code
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/GoldsteinPanel.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/GoldsteinPanel.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.jenkinsmonitor.gui;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -27,40 +27,33 @@ import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.text.JTextComponent;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.text.MessageFormat;
 
 public class GoldsteinPanel extends JPanel {
     public static final Logger LOGGER = LoggerFactory.getLogger(GoldsteinPanel.class);
 
-    String imgPath;
-
     public GoldsteinPanel() {
 
-        this.setLayout( new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        this.add( new JLabel("Wer den Jenkins kontrolliert, kontrolliert die Gegenwart !!!"));
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.add(new JLabel("Wer den Jenkins kontrolliert, kontrolliert die Gegenwart !!!"));
         try {
             final InputStream inputStream = getClass().getClassLoader().getResourceAsStream("1984EmmanuelGoldstein.jpg");
+            assert inputStream != null;
             final BufferedImage myPicture = ImageIO.read(inputStream);
             final JLabel picLabel = new JLabel(new ImageIcon(myPicture));
             this.add(picLabel);
             inputStream.close();
         } catch (IOException e) {
-            LOGGER.warn("Goldstein Panel nicht erzeugt: " + e);
+            LOGGER.warn(MessageFormat.format("Goldstein Panel nicht erzeugt: {0}", e));
         }
 
-        this.add( new JLabel("Wer die Vergangenheit kontrolliert, kontrolliert die Zukunft."));
-        this.add( new JLabel("Wer die Gegenwart kontrolliert, kontrolliert die Vergangenheit."));
-        this.add( new JLabel("— Georg Orwell (Author des Buches \"1984\")"));
+        this.add(new JLabel("Wer die Vergangenheit kontrolliert, kontrolliert die Zukunft."));
+        this.add(new JLabel("Wer die Gegenwart kontrolliert, kontrolliert die Vergangenheit."));
+        this.add(new JLabel("— Georg Orwell (Author des Buches \"1984\")"));
 
     }
 
-    public String getImgPath() {
-        return imgPath;
-    }
 }
