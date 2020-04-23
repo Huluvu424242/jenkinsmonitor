56ca8bdfe4f286167294e175661eb6ec694cba2d
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-23 22:00:52 +0200

    [feature/settings] Refactoring: Clean Code
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/Statusfenster.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/Statusfenster.java
@@ -31,7 +31,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
