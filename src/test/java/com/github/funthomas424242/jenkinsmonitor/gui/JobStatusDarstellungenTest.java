55ac2858df841b8d86866c35ecf8a3942906f252
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-25 17:04:06 +0200

    [feature/settings] Refactoring unused imports
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/gui/JobStatusDarstellungenTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/gui/JobStatusDarstellungenTest.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.jenkinsmonitor.gui;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -23,7 +23,6 @@ package com.github.funthomas424242.jenkinsmonitor.gui;
  */
 
 import com.github.funthomas424242.jenkinsmonitor.etc.NetworkHelper;
-import com.github.funthomas424242.jenkinsmonitor.jenkins.JenkinsClient;
 import com.github.funthomas424242.jenkinsmonitor.jenkins.JobStatus;
 import com.github.funthomas424242.jenkinsmonitor.jenkins.JobStatusBeschreibung;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,7 +30,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class JobStatusDarstellungenTest {
 
@@ -54,7 +53,7 @@ class JobStatusDarstellungenTest {
         jobsStatusBeschreibungen.put(jobsStatusBeschreibung0.getPrimaryKey(), jobsStatusBeschreibung0);
 
         final JobStatusDarstellungen jobStatusDarstellungen
-            =new JobStatusDarstellungen(jobsStatusBeschreibungen,clock);
+            = new JobStatusDarstellungen(jobsStatusBeschreibungen, clock);
         final Statusfenster statusArea = new Statusfenster(jobsStatusBeschreibungen);
 
         jobStatusDarstellungen.aktualisiereStatusfenster();
