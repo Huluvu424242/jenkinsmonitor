552f68d81b11f126fd953cb82222f9ca66eefeeb
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-26 17:23:46 +0200

    [master] Im Link zum Bugtracker den Filter auf Bugs aufgenommen
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/ContextMenu.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/ContextMenu.java
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 
 public class ContextMenu {
 
-    public static final String WEBSITE_JENKINSMONITOR_ISSUES = "https://github.com/FunThomas424242/jenkinsmonitor/issues";
+    public static final String WEBSITE_JENKINSMONITOR_ISSUES = "https://github.com/FunThomas424242/jenkinsmonitor/issues?q=is%3Aopen+is%3Aissue+label%3Abug";
     public static final String WEBSITE_JENKINSMONITOR = "https://github.com/FunThomas424242/jenkinsmonitor";
     public static final String ERR_COULD_NOT_OPEN_URL = "URL %s konnte nicht geöffnet werden";
 
