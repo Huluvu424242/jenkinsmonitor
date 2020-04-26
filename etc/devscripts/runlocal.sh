bcc645a426cbb211c5dd9a4b35eafdebcee6911f
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-26 15:22:47 +0200

    [release/0.1.1] snapshot entfernt
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- etc/devscripts/runlocal.sh
+++ etc/devscripts/runlocal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-"${JAVA_HOME}"/bin/java -Dlogback.configurationFile=filelogging.xml -jar ../../target/jenkinsmonitor-0.1.1-SNAPSHOT-jar-with-dependencies.jar
+"${JAVA_HOME}"/bin/java -Dlogback.configurationFile=filelogging.xml -jar ../../target/jenkinsmonitor-0.1.1-jar-with-dependencies.jar
 
