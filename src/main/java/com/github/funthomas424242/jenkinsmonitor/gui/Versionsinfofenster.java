4b1d630d1bf221c0ab96132f5b4e503b2f32f859
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-04-26 14:59:55 +0200

    [master] Version auf 0.1.1. gesetzt
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/Versionsinfofenster.java
+++ src/main/java/com/github/funthomas424242/jenkinsmonitor/gui/Versionsinfofenster.java
@@ -50,7 +50,7 @@ public class Versionsinfofenster extends JWindow {
         final JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 1));
         panel.add(new JLabel("<html>" +
-                "<h1>Jenkins Monitor Version: 0.0.3</h1>" +
+                "<h1>Jenkins Monitor Version: 0.1.1</h1>" +
                 "<p>Wer den Jenkins kontrolliert, kontrolliert die Gegenwart !!!</p>" +
                 "</html>"));
 
