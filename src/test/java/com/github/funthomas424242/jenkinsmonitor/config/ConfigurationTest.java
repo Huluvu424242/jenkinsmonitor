c0c24c1a079458d34186cd74d6de62169e07fe5f
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-08-29 22:09:12 +0200

    [feature/jacoco] Testfall ergänzt
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jenkinsmonitor/config/ConfigurationTest.java
+++ src/test/java/com/github/funthomas424242/jenkinsmonitor/config/ConfigurationTest.java
@@ -22,10 +22,10 @@ package com.github.funthomas424242.jenkinsmonitor.config;
  * #L%
  */
 
+import com.github.funthomas424242.jenkinsmonitor.etc.JavaSystemWrapper;
 import com.github.funthomas424242.jenkinsmonitor.jenkins.BasicAuthDaten;
 import com.github.funthomas424242.jenkinsmonitor.jenkins.JobBeschreibungen;
 import java.io.File;
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
@@ -110,25 +110,37 @@ class ConfigurationTest {
         });
     }
 
-//    @Test
-//    @DisplayName("Die Default Konfiguration wird aus ${HOMESHARE}/jenkinsmonitor.properties geladen")
-//    protected void useSharedDefaultConfigfile() throws NoSuchFieldException {
-//        final Map<String,String> envVars = System.getenv();
-//        final String tmpDir = System.getenv("TEMP");
-//        Field field = envVars.getClass().getDeclaredField("m");
-//        field.setAccessible(true);
-//        envVars.put("HOMESHARE",tmpDir);
-//        assertDoesNotThrow(() -> {
-//            final File defaultConfigurationsfile = Configuration.getDefaultConfigurationsfile();
-//            final Configuration configuration = new Configuration(defaultConfigurationsfile);
-//            assumeTrue(configuration != null);
-//            final File file = configuration.getConfigurationfile();
-//
-//            final String configurationsFilePath = file.getAbsolutePath().toString();
-//            final String expectedPath = tmpDir + File.separator + Configuration_CONFIGURATIONFILENAME;
-//            assertEquals(expectedPath, configurationsFilePath);
-//        });
-//    }
+    @Test
+    @DisplayName("Die Default Konfiguration wird aus ${HOMESHARE}/jenkinsmonitor.properties geladen")
+    protected void useSharedDefaultConfigfile() throws NoSuchFieldException {
+
+        final Map<String, String> envVars = System.getenv();
+        final String tmpDir = new File("src/test/resources/").getAbsolutePath().toString();
+        System.out.println("TMP DIR: " + tmpDir);
+        final JavaSystemWrapper.JavaSystemMock mock = new JavaSystemWrapper.JavaSystemMock() {
+
+            @Override
+            public String getenv(String name) {
+                if ("HOMESHARE".equals(name)) {
+                    return tmpDir;
+                } else {
+                    return null;
+                }
+            }
+        };
+
+        assertDoesNotThrow(() -> {
+            Configuration.setJavaSysteMock(mock);
+            final File defaultConfigurationsfile = Configuration.getDefaultConfigurationsfile();
+            final Configuration configuration = new Configuration(defaultConfigurationsfile);
+            assumeTrue(configuration != null);
+            final File file = configuration.getConfigurationfile();
+
+            final String configurationsFilePath = file.getAbsolutePath().toString();
+            final String expectedPath = tmpDir + File.separator + Configuration_CONFIGURATIONFILENAME;
+            assertEquals(expectedPath, configurationsFilePath);
+        });
+    }
 
 
     @Test
