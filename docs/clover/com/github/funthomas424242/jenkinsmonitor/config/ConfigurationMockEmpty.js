85cd9ff88d5d88ac6884b78969a4262e4c4ac52f
Huluvu424242 <Huluvu424242@users.noreply.github.com>
2023-01-21 17:46:43 +0000

    Autogenerated Documenation by github action
    Signed-off-by: Huluvu424242 <Huluvu424242@users.noreply.github.com>

--- docs/clover/com/github/funthomas424242/jenkinsmonitor/config/ConfigurationMockEmpty.js
+++ docs/clover/com/github/funthomas424242/jenkinsmonitor/config/ConfigurationMockEmpty.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":41,"id":843,"methods":[{"el":35,"sc":5,"sl":33},{"el":40,"sc":5,"sl":37}],"name":"ConfigurationMockEmpty","sl":29}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_15":{"methods":[{"sl":33},{"sl":37}],"name":"reloadOtherConfiguration","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]},"test_36":{"methods":[{"sl":33},{"sl":37}],"name":"initAfterGetPollOK","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]},"test_6":{"methods":[{"sl":33},{"sl":37}],"name":"initAfterGetJobBeschreibungenOK","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]}}
+clover.testTargets = {"test_21":{"methods":[{"sl":33},{"sl":37}],"name":"reloadOtherConfiguration","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]},"test_35":{"methods":[{"sl":33},{"sl":37}],"name":"initAfterGetPollOK","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]},"test_41":{"methods":[{"sl":33},{"sl":37}],"name":"initAfterGetJobBeschreibungenOK","pass":true,"statements":[{"sl":34},{"sl":38},{"sl":39}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [15, 6, 36], [15, 6, 36], [], [], [15, 6, 36], [15, 6, 36], [15, 6, 36], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [41, 21, 35], [41, 21, 35], [], [], [41, 21, 35], [41, 21, 35], [41, 21, 35], [], []]
