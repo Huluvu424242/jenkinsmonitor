683760074be24d37486502ca412adab7b53816fa
Huluvu424242 <Huluvu424242@users.noreply.github.com>
2023-01-21 18:17:18 +0000

    Autogenerated Documenation by github action
    Signed-off-by: Huluvu424242 <Huluvu424242@users.noreply.github.com>

--- docs/clover/clover.xml/com/github/funthomas424242/jenkinsmonitor/jenkins/JobNotFoundException.js
+++ docs/clover/clover.xml/com/github/funthomas424242/jenkinsmonitor/jenkins/JobNotFoundException.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":33,"id":802,"methods":[{"el":31,"sc":5,"sl":29}],"name":"JobNotFoundException","sl":27}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_35":{"methods":[{"sl":29}],"name":"getStatusGray","pass":true,"statements":[{"sl":30}]},"test_43":{"methods":[{"sl":29}],"name":"getValidJsonGray","pass":true,"statements":[{"sl":30}]}}
+clover.testTargets = {"test_2":{"methods":[{"sl":29}],"name":"getStatusGray","pass":true,"statements":[{"sl":30}]},"test_8":{"methods":[{"sl":29}],"name":"getValidJsonGray","pass":true,"statements":[{"sl":30}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [35, 43], [35, 43], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [8, 2], [8, 2], [], [], []]
